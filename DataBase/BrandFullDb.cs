2e326e4a99e1eaa649dcd79c6557cff975e0dfa9
chenjie <chenjie@manmanbuy.com>
2017-05-26 16:53:10 +0800

    xxxxxxx

---
+++ DataBase/BrandFullDb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Commons;
+using Entitys;
+using ServiceStack.OrmLite;
+
+namespace DataBase
+{
+    public class BrandFullDb : OrmLiteFactory
+    {
+        private static OrmLiteConnectionFactory _dbFactory;
+        public BrandFullDb()
+        {
+            _dbFactory = new OrmLiteConnectionFactory(ZnmDbConnectionString, SqlServerDialect.Provider);
+            using (var db = _dbFactory.OpenDbConnection())
+            {
+                db.CreateTable<BrandFull>();
+            }
+            using (var db = _dbFactory.OpenDbConnection())
+            {
+                db.CreateTable<AutoHomeBrand>();
+            }
+        }
+
+        public void AddBandInfo(List<BrandFull> list)
+        {
+
+            using (var db = _dbFactory.OpenDbConnection())
+            {
+                try
+                {
+                    db.InsertAll(list);
+                }
+                catch (Exception ex)
+                {
+                    LogServer.WriteLog(ex, "DBError");
+                }
+            }
+
+
+        }
+
+
+        public void AddBandInfo(List<AutoHomeBrand> list)
+        {
+
+            using (var db = _dbFactory.OpenDbConnection())
+            {
+                try
+                {
+                    db.InsertAll(list);
+                }
+                catch (Exception ex)
+                {
+                    LogServer.WriteLog(ex, "DBError");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Entitys/BrandFull.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.DataAnnotations;
+namespace Entitys
+{
+    public class BrandFull
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+        public int Pid { get; set; }
+        public int Level { get; set; }
+        public string Title { get; set; }
+        public string Letter { get; set; }
+        public string CountryCode { get; set; }
+        public string LevelCode { get; set; }
+        public string Logo { get; set; }
+        public int Flag { get; set; }
+        public int Hot { get; set; }
+        public string MoblieLogo { get; set; }
+        public string BrandList { get; set; }
+        public string Fcid { get; set; }
+        public string Pinyin { get; set; }
+    }
+
+    public class AutoHomeBrand
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+        public int Pid { get; set; }
+        public int Level { get; set; }
+        public string Title { get; set; }
+        public string Letter { get; set; }
+        public string CountryCode { get; set; }
+        public string CarType { get; set; }
+        public string Logo { get; set; }
+        public int Hot { get; set; }
+        public string MoblieLogo { get; set; }
+        public string Pinyin { get; set; }
+    }
+}
